1af920e53ee5d4c3dbd1dd6a287dbc5fa4b5ad7f
silvanet <silvanet@gmail.com>
2018-06-29 15:42:51 -0400

    comment out script

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //Babel coded by @SamAI-Software
-const projectName = 'survey-form';
-localStorage.setItem('example_project', 'Survey Form');
+//const projectName = 'survey-form';
+//localStorage.setItem('example_project', 'Survey Form');
